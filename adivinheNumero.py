52227684f7c48f7532f475c7f3c1bf4f4258636c
CarlosAlfredoOliveiraDeLima <carlos.alfredo.oliveira.de.lima@gmail.com>
2020-12-10 14:10:12 -0300

    Implementing the tips for the players about the location range of their numbers.

--- adivinheNumero.py
+++ adivinheNumero.py
@@ -1,24 +1,35 @@
 from random import randint
 
-def cueRandonNumberPC(max):
-    return randint(1, max)
+def cueRandonNumberPC(min, max):
+    return randint(min, max)
 
 
 def findNumber(rangeNumber):
     numberToFind = randint(1, rangeNumber)
     cueNumberUser = 0
     cueNumberPC = 0
-
-    while cueNumberUser != rangeNumber and cueNumberPC != rangeNumber:
+    minCuePC = 1
+    maxCuePC = rangeNumber
+    while cueNumberUser != numberToFind and cueNumberPC != numberToFind:
         cueNumberUser = int(input("Digite um palpite para encontrar o número secreto: "))
-        print(cueNumberUser)
-        cueNumberPC = cueRandonNumberPC(rangeNumber)
-        print(cueNumberPC)
+        if cueNumberUser > numberToFind:
+            print(f"\nOps, your number {cueNumberUser} is too high, try a smaller number\n")
+        elif cueNumberUser < numberToFind:
+            print(f"\nOps, your number {cueNumberUser} is too small, try a higher number\n")
+
+        cueNumberPC = cueRandonNumberPC(minCuePC, maxCuePC)
+        if cueNumberPC > numberToFind:
+            print(f'PC entered the number {cueNumberPC}, that was too high\n\n')
+            maxCuePC = cueNumberPC - 1
+        elif cueNumberPC < numberToFind:
+            print(f'PC entered the number {cueNumberPC}, that was too small\n\n')
+            minCuePC = cueNumberPC + 1
+
 
     if cueNumberUser == numberToFind:
-        print(f'Parabéns Jogador! Você acertou, o número secreto era {numberToFind}')
-    else:
-        print(f'Parabéns Computador! Você acertou, o número secreto era {numberToFind}')
+        print(f'\n\nParabéns Jogador! Você acertou, o número secreto era {numberToFind}')
+    if cueNumberPC == numberToFind:
+        print(f'\n\nParabéns Computador! Você acertou, o número secreto era {numberToFind}')
 
 
 rangeNumber = int(input("Digite um numero de range: "))
